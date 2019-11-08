e543cea14b9a102eecf730adb45101d7e5ae25e4
서주원 <jwseo001@khu.ac.kr>
2019-11-08 20:29:13 +0900

    Modify /user/index.js again

--- api/user/index.js
+++ api/user/index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('../../passport');
 
 router.get('/login/callback',passport.authenticate('kakao',{
     successRedirect:'/success',
